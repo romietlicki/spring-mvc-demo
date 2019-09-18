66373c67100ac7b400b9d3fe47ee41e85f3771da
romietlicki <rodrigo.mietlicki@mastercard.com>
2019-09-18 16:01:14 -0300

    update the student form and confirmation screen

--- spring-mvc-demo/WebContent/WEB-INF/view/student-form.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/student-form.jsp
@@ -24,6 +24,29 @@
 		<br>
 		<br>
 
+	Country:
+	<form:select path="country">
+		<form:options items="${student.countryOption}"/>
+	</form:select>
+	
+	<br>
+	<br>
+	
+	Favorite Language:
+	Java:<form:radiobutton path="favoriteLanguage" value="Java"/>
+	C#:<form:radiobutton path="favoriteLanguage" value="C#"/>
+	PHP::<form:radiobutton path="favoriteLanguage" value="PHP"/>
+	Ruby:<form:radiobutton path="favoriteLanguage" value="Ruby"/>
+	Delphi:<form:radiobutton path="favoriteLanguage" value="Delphi"/>
+	
+	<br>
+	<br>
+	
+	Operating Systems:
+	Linux: <form:checkbox path="operatingSystems" value="Linux"/>
+	Mac OS: <form:checkbox path="operatingSystems" value="Mac Os"/>
+	Windows: <form:checkbox path="operatingSystems" value="Windows"/>
+	
 		<input type="submit" value="Submit" />
 
 	</form:form>
--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
@@ -1,12 +1,24 @@
 package com.luv2code.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 	
 	private String firstName;
 	private String lastName;
+	private String country;
+	private LinkedHashMap<String, String> countryOption;
+	private String favoriteLanguage;
+	private String [] operatingSystems;
+	
 	
 	public Student(){
-		
+		countryOption = new LinkedHashMap<String, String>();
+		countryOption.put("BR", "Brazil");
+		countryOption.put("FR", "France");
+		countryOption.put("DE", "Germany");
+		countryOption.put("IN", "India");
+		countryOption.put("US", "United States of America");
 	}
 
 	public String getFirstName() {
@@ -23,6 +35,39 @@ public class Student {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOption() {
+		return countryOption;
+	}
+	
+
+	public void setCountryOption(LinkedHashMap<String, String> countryOption) {
+		this.countryOption = countryOption;
+	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public String [] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String [] operatingSystems) {
+		this.operatingSystems = operatingSystems;
 	}
 	
 
